47d0df0f62f732862a86e2e2fa51aa9a2c44289e
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-04-01 19:19:10 -0700

    optimze fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Raleway, Josefin_Sans, Overpass } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"], style: ["normal"] });
+const josefin_sans = Josefin_Sans({ subsets: ["latin"], style: ["italic"] });
+const overpass = Overpass({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          .header {
+            font-family: ${raleway.style.fontFamily};
+          }
+          .subheader {
+            font-family: ${josefin_sans.style.fontFamily};
+          }
+          .text {
+            font-family: ${overpass.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
