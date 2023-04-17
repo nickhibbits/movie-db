66245b34feaeaaf8c9e9672c22cdc0c6a2063e57
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-04-17 16:31:00 -0700

    refactor search results page

--- pages/results/index.tsx
+++ pages/results/index.tsx
@@ -3,10 +3,11 @@ import { Result } from "$/types/movieResults";
 
 import Card from "$/components/block/Card";
 
-import classes from "$/styles/Layout.module.scss";
 import ScrollContainer from "$/components/utility/ScrollContainer";
 import Layout from "$/components/composition/Layout";
 import TitleRow from "$/components/composition/TitleRow";
+
+import layoutClasses from "$/styles/Layout.module.scss";
 
 function Results({ results }: { results: any }) {
   const _results = JSON.parse(results);
@@ -15,32 +16,53 @@ function Results({ results }: { results: any }) {
   return (
     <Layout>
       {Object.entries(_results).map((result, i) => {
-        const titleType = result[0];
+        let titleType = "Other";
+
+        switch (result[0]) {
+          case "movie":
+            titleType = "Movies";
+            break;
+
+          case "tvMovie":
+            titleType = "TV Movies";
+            break;
+          case "podcastSeries":
+            titleType = "Podcasts";
+            break;
+          case "tvSeries":
+            titleType = "TV Series";
+            break;
+        }
+
+        result[0];
+
         const titles = result[1] as Result[];
 
-        return (
-          <TitleRow key={titleType}>
-            <h2>{titleType}</h2>
-            <ScrollContainer>
-              <ul className={classes.cardLayout}>
-                {titles.map((title) => {
-                  const { id, primaryImage, releaseYear, titleText } = title;
+        if (titles.length > 0) {
+          return (
+            <TitleRow key={titleType}>
+              <h2 className={layoutClasses.titleRowHeader}>{titleType}</h2>
+              <ScrollContainer>
+                <ul className={layoutClasses.cardLayout}>
+                  {titles.map((title) => {
+                    const { id, primaryImage, releaseYear, titleText } = title;
 
-                  return (
-                    <li key={id}>
-                      <Card
-                        id={id}
-                        primaryImage={primaryImage}
-                        releaseYear={releaseYear}
-                        titleText={titleText}
-                      />
-                    </li>
-                  );
-                })}
-              </ul>
-            </ScrollContainer>
-          </TitleRow>
-        );
+                    return (
+                      <li key={id}>
+                        <Card
+                          id={id}
+                          primaryImage={primaryImage}
+                          releaseYear={releaseYear}
+                          titleText={titleText}
+                        />
+                      </li>
+                    );
+                  })}
+                </ul>
+              </ScrollContainer>
+            </TitleRow>
+          );
+        }
       })}
     </Layout>
   );
