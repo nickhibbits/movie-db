3aa5ab8a12225580489e81574fccd49271b95f29
Nicholas Hibbits <nicholashibbits@gmail.com>
2023-04-24 18:31:29 -0700

    format title info, begin developing UI

--- pages/title/[titleId].tsx
+++ pages/title/[titleId].tsx
@@ -4,19 +4,57 @@ import { fetchData } from "$/utils/fetchData";
 
 import Link from "next/link";
 import Layout from "$/components/composition/Layout";
+import { TitleInfo } from "$/types/titleInfo";
 
 function TitleId() {
-  const [titleInfo, setTitleInfo] = useState();
+  const [titleInfo, setTitleInfo] = useState<TitleInfo>();
   const router = useRouter();
   const titleId = router.query.titleId;
+
+  const formatTitleInfo = (data: any) => {
+    const {
+      genres,
+      id,
+      plot,
+      primaryImage,
+      ratingsSummary,
+      releaseDate,
+      releaseYear,
+      runtime,
+      titleText,
+      titleType,
+    } = data.base_info;
+
+    const extendedCast = data.extendedCast.cast.edges.map((castMember: any) => {
+      const { characters, episodeCredits, name } = castMember;
+
+      return { characters, episodeCredits, name };
+    });
+    let titleInfo: TitleInfo = {
+      extendedCast,
+      genres,
+      id,
+      plot,
+      primaryImage,
+      ratingsSummary,
+      releaseDate,
+      releaseYear,
+      runtime,
+      titleText,
+      titleType,
+    };
+
+    return titleInfo;
+  };
 
   useEffect(() => {
     if (titleId) {
       const getTitleInfo = async () => {
         const url = `/api/titles/searchTitlesById?titleId=${titleId}`;
-        const data = await fetchData(url);
+        const data = await fetchData(url).then((data) => formatTitleInfo(data));
 
         console.log("data", data);
+        console.log("🟢 titleInfo set");
         return data;
       };
 
@@ -24,10 +62,13 @@ function TitleId() {
     }
   }, [titleId]);
 
+  console.log("titleInfo", titleInfo);
+
   return (
     <Layout>
-      <img src="" alt="" />
-      <Link href="/actor/test-actor-id">{titleInfo}</Link>
+      <img src={titleInfo?.primaryImage.url} alt="" />
+
+      <Link href="/actor/test-actor-id">Ello</Link>
     </Layout>
   );
 }
